4bab6e5d75640b907bddd2b0729339c54203b6e6
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-09-26 19:51:18 -0300

    fifth commit

---
+++ src/DominoOBI2019462/Main.java
@@ -0,0 +1,14 @@
+package DominoOBI2019462;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Integer N = sc.nextInt();
+		System.out.println(((N+1)*(N+2))/2);
+
+	}
+
+}
