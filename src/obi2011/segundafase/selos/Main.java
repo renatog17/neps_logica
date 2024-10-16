61dca6b46cdeeb7803d542ed466895175ceba71d
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-16 13:25:53 -0300

    new questions added

---
+++ src/obi2011/segundafase/selos/Main.java
@@ -0,0 +1,17 @@
+package obi2011.segundafase.selos;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Integer numero = sc.nextInt();
+		
+		for (int i = 0; i < numero; i++) {
+			
+		}
+
+	}
+
+}
