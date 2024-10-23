2c4d13acfc435bf1195cd72ac8a560d0a948df83
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-23 12:39:47 -0300

    obi 2021 primeira fase idade de camila

--- src/obi2021/primeirafase/idadedecamila/Main.java
+++ src/obi2021/primeirafase/idadedecamila/Main.java
@@ -10,9 +10,29 @@ public class Main {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		
+		int max;
+		int min;
+		if(a>b && a>c) {
+			max = a;
+		}else {
+			if(b>c) {
+				max = b;
+			}else {
+				max = c;
+			}
+		}
 		
-	
-
+		if(a<b && a<c) {
+			min = a;
+		}else {
+			if(b<c) {
+				min = b;
+			}else {
+				min = c;
+			}
+		}
+		System.out.println(a +b +c - max-min);
+		
 	}
 
 }
