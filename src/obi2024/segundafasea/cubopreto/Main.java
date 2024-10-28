c73f9723ca48f87773a2f9f5c35f5932685ca1d0
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-28 12:41:29 -0300

    implementing base scope obi 2024 segunda fase a

---
+++ src/obi2024/segundafasea/cubopreto/Main.java
@@ -0,0 +1,14 @@
+package obi2024.segundafasea.cubopreto;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println((int)Math.pow(n-2, 3));
+		//System.out.println((n)*6);
+	}
+
+}
