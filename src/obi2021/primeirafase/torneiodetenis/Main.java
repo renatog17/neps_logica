e40a98283739bc0537a9a4f25ca4272592a9f015
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-30 11:29:59 -0300

    obi2021 primeirafase torneio de tenis

--- src/obi2021/primeirafase/torneiodetenis/Main.java
+++ src/obi2021/primeirafase/torneiodetenis/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		int cont = 0;
+		for (int i = 0; i < 6; i++) {
+			if(sc.next().equals("V")) cont++;
+		}
+		
+		if(cont>=5) System.out.println(1);
+		else if(cont>=3) System.out.println(2);
+		else if(cont>=1) System.out.println(3);
+		else System.out.println(-1);
 	}
 
 }
