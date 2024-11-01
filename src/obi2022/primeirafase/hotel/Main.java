2fce856fa1b0a9353608d75c3238285af10ae677
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-11-01 16:40:01 -0300

    obi2022 primeira fase hotel

--- src/obi2022/primeirafase/hotel/Main.java
+++ src/obi2022/primeirafase/hotel/Main.java
@@ -6,7 +6,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		int d = sc.nextInt();
+		int a = sc.nextInt();
+		int n = sc.nextInt();
+		
+		int qtdDias = 32-n;
+		
+		int aumento;
+		
+		if(31-qtdDias>=15) {
+			aumento = 14*a;
+		}else {
+			aumento = (31-qtdDias)*a;
+		}
+		
+		System.out.println((d+aumento)*qtdDias);
 	}
-
 }
