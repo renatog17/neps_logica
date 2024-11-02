c5a4c64b64289c60894a20092f1742c069e2876d
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-11-01 23:02:31 -0300

    irmaoes obi 2020 added

---
+++ src/obi2020/primeirafase/b/irmaos/Main.java
@@ -0,0 +1,14 @@
+package obi2020.primeirafase.b.irmaos;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(b-a+b);
+	}
+
+}
--- src/obi2022/primeirafase/show/Main.java
+++ src/obi2022/primeirafase/show/Main.java
@@ -6,7 +6,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		int a = sc.nextInt();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int saida = n;
+		
+		for (int i = n; i > 0; i--) {
+			int maiorSequenciaLivre = 0;
+			for (int j = 0; j < m; j++) {
+				int sequenciaLivre = 0;
+				if(sc.nextInt()==0) {
+					sequenciaLivre++;
+				}else {
+					if(sequenciaLivre>maiorSequenciaLivre)
+						maiorSequenciaLivre = sequenciaLivre;
+					sequenciaLivre = 0;
+				}
+			}
+			if(maiorSequenciaLivre>=a) {
+				saida = n;
+			}
+		}
+		System.out.println(saida);
 	}
 
 }
