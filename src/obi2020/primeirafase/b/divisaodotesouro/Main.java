49c94f5c087e892d659dc1a7cc29ff7642dc9a3e
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-11-03 15:54:44 -0300

    obi2020 primeira fase b divisao do tesouro

---
+++ src/obi2020/primeirafase/b/divisaodotesouro/Main.java
@@ -0,0 +1,15 @@
+package obi2020.primeirafase.b.divisaodotesouro;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int m = sc.nextInt()+2;
+		System.out.println((t/m)*2);
+
+	}
+
+}
