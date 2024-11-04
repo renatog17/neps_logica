115187825425900c25f93485eddf5160ab199f96
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-11-04 10:56:15 -0300

    obi 2024 segunda fase a cubopreto

--- src/obi2024/segundafasea/cubopreto/Main.java
+++ src/obi2024/segundafasea/cubopreto/Main.java
@@ -7,7 +7,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println((int)Math.pow(n-2, 3));
+		
+		if(n>=3) {
+			System.out.println((int)Math.pow(n-2, 3));
+			System.out.println((n-2)*(n-2)*6);
+			System.out.println((n-2)*12);
+			System.out.println(8);
+		}else {
+			System.out.println(0);
+			System.out.println(0);
+			System.out.println(0);
+			System.out.println(8);
+		}
 		//System.out.println((n)*6);
 	}
 
