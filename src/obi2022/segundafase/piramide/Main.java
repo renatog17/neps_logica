94569b646c0f9f09861410268c86adc9579e6403
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-12-08 23:04:09 -0300

    piramide obi 2022 segunda fase

--- src/obi2022/segundafase/piramide/Main.java
+++ src/obi2022/segundafase/piramide/Main.java
@@ -1,11 +1,30 @@
 package obi2022.segundafase.piramide;
-
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.close();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-	}
+        int[][] piramide = new int[N][N];
 
+        for (int camada = 1; camada <= (N + 1) / 2; camada++) {
+            int inicio = camada - 1;
+            int fim = N - camada;
+
+            for (int i = inicio; i <= fim; i++) {
+                for (int j = inicio; j <= fim; j++) {
+                    piramide[i][j] = camada;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(piramide[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
